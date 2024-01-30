7d4a18539cc5eb7e7ca9495423907cbcf6d28938
vinna8 <vdovina.innusja@mail.ru>
2024-01-30 18:50:57 +0700

    navigation menu layout

---
+++ src/components/Header/StyledHeader.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const HeaderNavigation = styled.div`
+    margin: auto;
+    max-width: 1200px; 
+    padding: 22px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color: #344051;
+`;
+
+export const HeaderNavigationItemLeft = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 32px;
+`;
+
+export const HeaderNavigationItemRight = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 16px;
+`;
+
+export const Line = styled.div`
+    border-bottom: 1px solid #CED2DA;
+`
